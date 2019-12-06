5c5f8f6493e132674cff5dcaa1417e5474a08289
teb3716 <56224687+teb3716@users.noreply.github.com>
2019-12-06 22:48:51 +0900

    Update MinHeap.h

--- MinHeap.h
+++ MinHeap.h
@@ -1,20 +1,79 @@
-#ifndef RESULT_H
-#define RESULT_H
+#ifndef MIN_HEAP_H
+#define MIN_HEAP_H
 
-enum Result
+#include <utility>
+#include <vector>
+
+template<typename TKey, typename TValue>
+class MinHeap
 {
-    Success = 0,
+private:
+    // array for the elements which should be heap-sorted
+    std::vector<std::pair<TKey, TValue>> m_vec;
 
-    CommandFileNotExist = 100,
-    LoadFileNotExist,
+public:
+    MinHeap() {}
 
-    VertexKeyNotExist = 200,
-    InvalidVertexKey,
-    GraphNotExist,
-    InvalidAlgorithm,
-    NegativeCycleDetected,
+    /// <summary>
+    /// insert key-value pair
+    /// </summary>
+    ///
+    /// <param name="key">
+    /// the key that is used for sorting
+    /// </param>
+    ///
+    /// <param name="value">
+    /// the value that is managed in this heap
+    /// </param>
+    void Push(TKey key, TValue value);
+    /// <summary>
+    /// remove the minimum element
+    /// </summary>
+    void Pop();
+    /// <summary>
+    /// get the minimum element
+    /// </summary>
+    ///
+    /// <returns>
+    /// the minimum element
+    /// </returns>
+    std::pair<TKey, TValue> Top();
+    /// <summary>
+    /// get the key-value pair which the value is the same as the target
+    /// </summary>
+    ///
+    /// <returns>
+    /// the key-value pair which the value is the same as the target
+    /// </returns>
+    std::pair<TKey, TValue> Get(TValue target);
+    /// <summary>
+    /// check whether this heap is empty or not
+    /// </summary>
+    ///
+    /// <returns>
+    /// true if this heap is empty
+    /// </returns>
+    bool IsEmpty();
+    /// <summary>
+    /// change the key of the node which the value is the target.<para/>
+    /// In general, the newKey should be smaller than the old key.<para/>
+    /// </summary>
+    ///
+    /// <parma name="target">
+    /// the target to change the key
+    /// </param>
+    ///
+    /// <param name="newKey">
+    /// new key for the target
+    /// </param>
+    void DecKey(TValue target, TKey newKey);
 
-    NonDefinedCommand = 300,
+private:
+    /// <summary>
+    /// heap-sort, heapify.<para/>
+    /// this function can be called recursively
+    /// </summary>
+    void Heapify(int index);
 };
 
 #endif
